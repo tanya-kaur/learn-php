3429e0606325f06756cb3cef15e680fa61ad0413
tanya-kaur <51701390+tanya-kaur@users.noreply.github.com>
2019-07-17 09:48:54 +0530

    php files

--- register.php
+++ register.php
@@ -1,2 +1,21 @@
 <?php
-print_r($_GET);
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$dbh = new PDO('mysql:host=localhost;dbname=gokart', 'root', '');
+
+$stmt = $dbh->prepare("INSERT INTO users(name, email, passwd) VALUES(?, ?, ?)");
+
+//sql injection  
+
+if($stmt->execute([$name, $email, $password]) == 1){
+    echo 'Registered successfully.';
+}else{
+    echo 'REgistration failed. Please try again.';
+}
+
+//close
+$stmt = null;
+$dbh = null;
+
